f0f72a5148a38ee8ae96f28b48de5a96a6722de3
unknown <jason.rettinger@gmail.com>
2022-09-13 10:38:18 -0400

    added function select and answers

--- Develop/index.js
+++ Develop/index.js
@@ -1,5 +1,33 @@
-const Manager = require('./lib/Manager');
-const Engineer = require('./lib/Engineer');
-const Intern = require('./lib/Intern');
+const D = require("./src/renderHtml");
+const { Engineer } = require("./lib/Engineer");
+const { Intern } = require("./lib/Intern");
+const { Manager } = require("./lib/Manager");
 
-// TODO: CODE GOES HERE
+const inquirer = require("inquirer");
+
+const memberData = [];
+
+function Select() {
+    inquirer
+      .prompt([
+        {
+          type: "rawlist",
+          name: "menu",
+          message: "Please Select Employee Type",
+          choices: ["Engineer", "Intern", "Exit"],
+        },
+      ])
+      .then((answers) => {
+        switch (answers.menu) {
+          case "Engineer":
+            InputEngineer();
+            break;
+          case "Intern":
+            InputIntern();
+            break;
+          case "Exit":
+            D.saveAsHtml(memberData);
+            break;
+        }
+      });
+  }
